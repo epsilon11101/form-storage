97504907dbf76e9ec9b3e1958c9047fb3b8356f4
sa101129 <sw.frontend.developer@gm.traza.digital>
2025-10-08 14:10:54 -0600

    style:theme configuration

---
+++ src/theme/colors.ts
@@ -0,0 +1,59 @@
+
+import { type SimplePaletteColorOptions } from '@mui/material'
+
+const PRIMARY: SimplePaletteColorOptions = {
+  main: '#4996C6',
+}
+
+const SECONDARY: SimplePaletteColorOptions = {
+  main: '#7CC6D8',
+}
+
+const TERTIARY: SimplePaletteColorOptions = {
+  main: '#FF3E8D',
+  contrastText: "#FFF"
+}
+
+const ACCENT: SimplePaletteColorOptions = {
+  main: '#266FB6',
+}
+
+const TITLE_OUTLINE: SimplePaletteColorOptions = {
+  main: '#0F2A5B',
+}
+
+const ERROR: SimplePaletteColorOptions = {
+  main: '#D32F2F',
+}
+const WARNING: SimplePaletteColorOptions = {
+  main: '#FF7F30',
+}
+const INFO: SimplePaletteColorOptions = {
+  main: '#2196F3',
+}
+const SUCCESS: SimplePaletteColorOptions = {
+  main: '#2E7D32',
+}
+
+const FOCUS: SimplePaletteColorOptions = {
+  main: '#E5F6FD',
+}
+
+export const SURFACE = "#101010"
+export const SURFACE_CONTAINER = '#DCDCDC'
+export const ESTADOS_INFO = '#2196F3'
+export const OUTLINE = '#646464'
+export const OUTLINE_VARIANT = '#A0A0A0'
+
+export {
+  PRIMARY,
+  SECONDARY,
+  TERTIARY,
+  ACCENT,
+  TITLE_OUTLINE,
+  ERROR,
+  WARNING,
+  INFO,
+  SUCCESS,
+  FOCUS,
+}
---
+++ src/theme/theme.ts
@@ -0,0 +1,251 @@
+import "@fontsource/poppins/400.css";
+import "@fontsource/poppins/700.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
+
+import { createTheme, type Palette, type PaletteColor } from '@mui/material/styles'
+
+import {
+  PRIMARY,
+  SECONDARY,
+  TERTIARY,
+  ACCENT,
+  TITLE_OUTLINE,
+  ERROR,
+  WARNING,
+  INFO,
+  SUCCESS,
+  FOCUS,
+} from '@/theme/colors'
+
+import { type CSSProperties } from 'react'
+
+
+
+// Define custom types for palette keys
+export type PaletteKey = keyof {
+  [Key in keyof Palette as Palette[Key] extends PaletteColor
+  ? Key
+  : never]: true
+}
+// Extend Palette and PaletteOptions interfaces to include custom color keys
+declare module '@mui/material/styles' {
+  interface Palette {
+    tertiary: Palette['primary']
+    accent: Palette['primary']
+    titleOutline: Palette['primary']
+    focus: Palette['primary']
+  }
+
+  interface PaletteOptions {
+    tertiary?: PaletteOptions['primary']
+    accent?: PaletteOptions['primary']
+    titleOutline?: PaletteOptions['primary']
+    focus?: PaletteOptions['primary']
+  }
+}
+// Define component-specific color props
+// interface ComponentColorProps {
+//   tertiary?: true
+//   focus?: true
+//   accent?: true
+//   titleOutline?: true
+// }
+declare module '@mui/material/Button' {
+  interface ButtonPropsColorOverrides {
+    tertiary?: true
+    focus?: true
+    accent?: true
+    titleOutline?: true
+  }
+}
+
+declare module '@mui/material/CircularProgress' {
+  interface CircularProgressPropsColorOverrides {
+    tertiary?: true
+    focus?: true
+    accent?: true
+    titleOutline?: true
+  }
+}
+// TYPOGRAPHY VARIANTS
+declare module '@mui/material/styles' {
+  interface TypographyVariants {
+    display: CSSProperties
+    navLink: CSSProperties
+    link: CSSProperties
+    body: CSSProperties
+    bodyImp: CSSProperties
+    info: CSSProperties
+    textTitles: CSSProperties
+  }
+
+  interface TypographyPropsColorOverrides {
+    tertiary?: true
+    focus?: true
+    accent?: true
+    titleOutline?: true
+  }
+  // allow configuration using `createTheme()`
+  interface TypographyVariantsOptions {
+    display?: CSSProperties
+    navLink?: CSSProperties
+    link?: CSSProperties
+    body?: CSSProperties
+    bodyImp?: CSSProperties
+    info?: CSSProperties
+    textTitles?: CSSProperties
+
+  }
+}
+
+// Update the Typography's variant prop options
+declare module '@mui/material/Typography' {
+  interface TypographyPropsVariantOverrides {
+    display: true
+    navLink: true
+    link: true
+    body: true
+    bodyImp: true
+    info: true
+    textTitles: true
+  }
+}
+
+/**
+ * Function to create and customize MUI theme based on current locale.
+ * @returns Customized MUI theme object.
+ */
+export const theme = createTheme({
+  components: {
+    MuiTypography: {
+      defaultProps: {
+        variantMapping: {
+          body: 'p',
+          bodyImp: 'p',
+        },
+      },
+    },
+  },
+  typography: {
+    fontFamily: "Roboto, Arial, sans-serif",
+    h1: {
+      fontFamily: "Poppins, Arial, sans-serif",
+      fontSize: 42,
+      fontWeight: 700,
+      lineHeight: "normal",
+      fontStyle: "normal",
+      color: "#101010",
+    },
+    h2: {
+      fontFamily: "Poppins, Arial, sans-serif",
+      fontSize: 38,
+      fontWeight: 700,
+      lineHeight: "normal",
+      fontStyle: "normal",
+      color: "#101010",
+    },
+    h3: {
+      fontFamily: "Poppins, Arial, sans-serif",
+      fontSize: 30,
+      fontWeight: 700,
+      lineHeight: "normal",
+      fontStyle: "normal",
+      color: "#101010",
+    },
+    h4: {
+      fontFamily: "Poppins, Arial, sans-serif",
+      fontSize: 22,
+      fontWeight: 700,
+      lineHeight: "normal",
+      fontStyle: "normal",
+      color: "#101010",
+    },
+    button: {
+      fontFamily: "Roboto, Arial, sans-serif",
+      fontWeight: 500,
+      lineHeight: "normal",
+      fontStyle: "normal",
+      color: "#101010",
+    },
+    navLink: {
+      fontFamily: "Roboto, Arial, sans-serif",
+      fontSize: 18,
+      fontWeight: 600,
+      lineHeight: "normal",
+      fontStyle: "normal",
+      color: "#101010",
+    },
+    link: {
+      fontFamily: "Roboto, Arial, sans-serif",
+      fontSize: 16,
+      fontWeight: 400,
+      lineHeight: "normal",
+      fontStyle: "normal",
+      color: "#101010",
+    },
+    body: {
+      fontFamily: "Roboto, Arial, sans-serif",
+      fontSize: 16,
+      fontWeight: 400,
+      lineHeight: "normal",
+      fontStyle: "normal",
+      color: "#646464",
+    },
+    bodyImp: {
+      fontFamily: "Roboto, Arial, sans-serif",
+      fontSize: 16,
+      fontWeight: 600,
+      lineHeight: "normal",
+      fontStyle: "normal",
+      color: "#101010",
+    },
+    info: {
+      fontFamily: "Roboto, Arial, sans-serif",
+      fontSize: 14,
+      fontWeight: 500,
+      lineHeight: "normal",
+      fontStyle: "normal",
+      color: "#101010",
+    },
+    caption: {
+      fontFamily: "Roboto, Arial, sans-serif",
+      fontSize: 14,
+      fontWeight: 400,
+      lineHeight: "normal",
+      fontStyle: "normal",
+      color: "#101010",
+    },
+    textTitles: {
+      fontFamily: "Roboto, Arial, sans-serif",
+      fontSize: 10,
+      fontWeight: 400,
+      lineHeight: "normal",
+      fontStyle: "normal",
+      color: "#101010",
+    },
+    display: {
+      fontFamily: "Poppins, Arial, sans-serif",
+      fontSize: 38,
+      fontWeight: 400,
+      lineHeight: "normal",
+      fontStyle: "normal",
+      color: "#101010",
+    },
+
+  },
+  palette: {
+    primary: PRIMARY,
+    secondary: SECONDARY,
+    tertiary: TERTIARY,
+    error: ERROR,
+    success: SUCCESS,
+    warning: WARNING,
+    info: INFO,
+    focus: FOCUS,
+    accent: ACCENT,
+    titleOutline: TITLE_OUTLINE,
+  },
+})
