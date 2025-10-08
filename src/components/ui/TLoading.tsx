5d8983dfee22816fdcbc213356018de902b21087
sa101129 <sw.frontend.developer@gm.traza.digital>
2025-10-08 14:13:21 -0600

    feat: loading component

---
+++ src/components/ui/TLoading.tsx
@@ -0,0 +1,28 @@
+import {
+  CircularProgress,
+  type CircularProgressProps,
+  Stack,
+  type StackProps,
+} from '@mui/material'
+import type { FC } from 'react'
+
+
+interface TLoadingProps extends StackProps {
+  progressProps?: CircularProgressProps
+}
+
+const TLoading: FC<TLoadingProps> = ({ progressProps, ...rest }) => {
+  return (
+    <Stack
+      width="100vw"
+      height="100vh"
+      justifyContent="center"
+      alignItems="center"
+      {...rest}
+    >
+      <CircularProgress color="titleOutline" {...progressProps} />
+    </Stack>
+  )
+}
+
+export default TLoading
