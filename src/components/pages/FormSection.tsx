50981cbd18de022d0cf9a34d492ce67e558d9d6b
sa101129 <sw.frontend.developer@gm.traza.digital>
2025-10-09 17:31:31 -0600

    fix: add schema, uischema from reading file

--- src/components/pages/FormSection.tsx
+++ src/components/pages/FormSection.tsx
@@ -1,44 +1,28 @@
 import type { RJSFSchema } from "@rjsf/utils";
 import TForm from "../ui/rjsf/TForm";
 import Button from "@mui/material/Button";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import validator from '@rjsf/validator-ajv8';
-
-const schema: RJSFSchema = {
-  title: 'Test form',
-  type: 'object',
-  properties: {
-    name: { type: 'string' },
-    age: { type: 'number' },
-    birthday: {
-      type: 'string',
-      format: 'date',
-      title: 'Fecha de nacimiento',
-    },
-  },
-  required: ["name", "birthday"],
-};
-
-const uiSchema = {
-  birthday: {
-    "ui:widget": "DatePickerWidget",
-  },
-};
+import useReadDocument from "@/stores/useReadDocument";
 
 const FormSection = () => {
-  const [parsedSchema, setParsedSchema] = useState<RJSFSchema>(schema);
+
+  const { schema, uiSchema, formData, setFormData } = useReadDocument()
+
   return (
     <TForm
-      schema={parsedSchema}
+      schema={schema}
       uiSchema={uiSchema}
       validator={validator}
       noHtml5Validate
       liveValidate={false}
       showErrorList={false}
+      formData={formData}
+      onChange={e => setFormData(e.formData)}
     >
       <Button type="submit" variant="contained" color="primary" >Enviar</Button>
-    </TForm>
+    </TForm >
   )
 }
 
-export default FormSection    
+export default FormSection   
