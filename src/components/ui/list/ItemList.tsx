8d7a49897273935610d065a2e21e3e208f553636
sa101129 <sw.frontend.developer@gm.traza.digital>
2025-10-09 14:14:47 -0600

    feature: add ondelete and edit mutations

--- src/components/ui/list/ItemList.tsx
+++ src/components/ui/list/ItemList.tsx
@@ -1,6 +1,6 @@
-import { useState, type FC, type MouseEventHandler, type ReactNode } from "react"
+import { useState, type FC, type ReactNode } from "react"
 import type { FolderStructureType } from "./types"
-import { Collapse, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Menu, Stack } from "@mui/material";
+import { ListItem, ListItemButton, ListItemIcon, ListItemText, Menu, Stack } from "@mui/material";
 
 import { OUTLINE_VARIANT, SURFACE } from "@/theme/colors";
 import ArrowRightIcon from '@mui/icons-material/ArrowRight';
@@ -11,6 +11,10 @@ import { TMenu, TMenuItem } from "../TMenu";
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
 import { ItemListItem } from "./ItemListItem";
+import { useEditingField } from "../editingField/useEditingField";
+import { TEditingField } from "../editingField/TEditingField";
+import { useDeleteGroup, useEditGroup } from "@/api/hooks/useGroups";
+import { TProgress } from "../TProgress";
 
 interface Props {
   jsonItem: FolderStructureType[number]
@@ -20,6 +24,21 @@ interface Props {
 
 
 const ItemList: FC<Props> = ({ jsonItem, parentIcon, childrenIcon }) => {
+
+
+  const { mutate: onEditGroupMutation, isPending } = useEditGroup()
+  const { mutate: onDeleteGroupMutation, isPending: isDeletePending, isError, isSuccess } = useDeleteGroup()
+
+  const deleteStatus = (isSuccess || isDeletePending) && !isError
+
+  const { isEditing, setIsEditing, onChangeHandler, onKeyEnterHandler, value } = useEditingField({
+    initialValue: jsonItem.parent_name,
+    onEnter: onMutateHandler
+  })
+
+
+
+  const { } = useDeleteGroup()
 
   const [open, setOpen] = useState(false);
   const [openDialog, setOpenDialog] = useState(false)
@@ -36,6 +55,28 @@ const ItemList: FC<Props> = ({ jsonItem, parentIcon, childrenIcon }) => {
     setOpenDialog(false);
   }
 
+  const onEditingHandler = () => {
+    setIsEditing(true)
+  }
+
+  function onMutateHandler() {
+    onEditGroupMutation({
+      id: jsonItem.id,
+      name: value
+    }, {
+      onError: () => setIsEditing(false),
+      onSuccess: () => setIsEditing(false)
+    })
+  }
+
+  const onDeleteHandler = () => {
+    onDeleteGroupMutation({
+      id: jsonItem.id
+    })
+  }
+
+
+
   return (
     <>
 
@@ -43,57 +84,91 @@ const ItemList: FC<Props> = ({ jsonItem, parentIcon, childrenIcon }) => {
         <ListItem
 
           secondaryAction={
-            <Stack direction="row" gap={1} alignItems="center" width="100%" >
-              <TMenu>
-                {({ popupStateHandler, bindMenuProps }) =>
-                  <Menu {...bindMenuProps}>
-                    <TMenuItem
-                      popupState={popupStateHandler}
-                      onClick={onOpenDialogHandler}
-                      icon={<AddIcon color="primary" />}
-                      title="Agregar"
-                      sx={{ "&:hover": { bgcolor: "#eaf6fb" } }}
-                    />
+            !isEditing ?
+              <Stack direction="row" gap={1} alignItems="center" width="100%" >
+                {isPending ? null :
+                  <TMenu>
+                    {({ popupStateHandler, bindMenuProps }) =>
+                      <Menu {...bindMenuProps}>
+                        <TMenuItem
+                          popupState={popupStateHandler}
+                          onClick={onOpenDialogHandler}
+                          icon={<AddIcon color="primary" />}
+                          title="Agregar"
+                          sx={{ "&:hover": { bgcolor: "#eaf6fb" } }}
+                        />
 
-                    <TMenuItem
-                      popupState={popupStateHandler}
-                      onClick={() => { }}
-                      icon={<EditIcon color="primary" fontSize="small" />}
-                      title="Editar"
-                      sx={{ "&:hover": { bgcolor: "#eaf6fb" } }}
-                    />
-                    <TMenuItem
-                      popupState={popupStateHandler}
-                      onClick={() => { }}
-                      icon={<DeleteIcon color="error" fontSize="small" />}
-                      title="Eliminar"
-                      sx={{ "&:hover": { bgcolor: " #fbeaea" } }} />
-                  </Menu>
-                }
-              </TMenu>
-            </Stack>
+                        <TMenuItem
+                          popupState={popupStateHandler}
+                          onClick={onEditingHandler}
+                          icon={<EditIcon color="primary" fontSize="small" />}
+                          title="Editar"
+                          sx={{ "&:hover": { bgcolor: "#eaf6fb" } }}
+                        />
+                        <TMenuItem
+                          popupState={popupStateHandler}
+                          onClick={onDeleteHandler}
+                          icon={<DeleteIcon color="error" fontSize="small" />}
+                          title="Eliminar"
+                          sx={{ "&:hover": { bgcolor: " #fbeaea" } }} />
+                      </Menu>
+                    }
+                  </TMenu>}
+              </Stack> : null
           }
           dense
           disablePadding
-        >
-          <ListItemIcon sx={{ minWidth: 24 }}>{parentIcon}</ListItemIcon>
-          <ListItemText primary={jsonItem.parent_name} primaryTypographyProps={{
-            noWrap: true,
-            variant: "caption",
-            color: SURFACE,
-            width: 150
-          }} />
-          {open ? <ArrowDropDownIcon sx={{ color: OUTLINE_VARIANT }} /> : <ArrowRightIcon sx={{ color: OUTLINE_VARIANT }} />}
+        > {
+            <>
+              {
+                isPending ? (
+                  <TProgress />
+                ) : (
+                  <>
+                    <ListItemIcon sx={{ minWidth: 24 }}>{parentIcon}</ListItemIcon>
 
+                    {isEditing ? (
+                      <TEditingField
+                        value={value}
+                        onChange={onChangeHandler}
+                        onKeyDown={onKeyEnterHandler}
+                      />
+                    ) : (
+                      <>
+                        <ListItemText
+                          primary={deleteStatus ? "Eliminando" : value}
+                          primaryTypographyProps={{
+                            noWrap: true,
+                            variant: "caption",
+                            width: 150,
+                            sx: {
+                              color: theme => deleteStatus ? theme.palette.error.main : SURFACE,
+                            }
+                          }}
+                        />
+                        {open ? (
+                          <ArrowDropDownIcon sx={{ color: OUTLINE_VARIANT }} />
+                        ) : (
+                          <ArrowRightIcon sx={{ color: OUTLINE_VARIANT }} />
+                        )}
+                      </>
+                    )}
+                  </>
+                )
+              }
+            </>}
         </ListItem>
 
-      </ListItemButton>
+      </ListItemButton >
 
 
-      {jsonItem.children.map((child) => (
-        <ItemListItem key={child.id} {...child} in={open} icon={childrenIcon} />
-      ))}
-      <NewJSONDialog title={`Agregar json para ${jsonItem.parent_name}`} open={openDialog} onClose={onCloseDialogHandler} />
+      {
+        jsonItem.children.map((child) => (
+          <ItemListItem key={child.id} {...child} in={open} icon={childrenIcon} />
+        ))
+      }
+      < NewJSONDialog title={`Agregar json para ${jsonItem.parent_name}`
+      } open={openDialog} onClose={onCloseDialogHandler} />
     </>
 
   )
