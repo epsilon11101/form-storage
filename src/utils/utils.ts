e7e30676270274ef72556b34f695c26ff9bf1921
sa101129 <sw.frontend.developer@gm.traza.digital>
2025-10-10 10:44:55 -0600

    feat: add parse sche to string function

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,3 +1,5 @@
+import type { RJSFSchema, UiSchema } from "@rjsf/utils"
+
 export const readFileAsText = (file: File): Promise<string> => {
   return new Promise((resolve, reject) => {
     const reader = new FileReader()
@@ -5,4 +7,23 @@ export const readFileAsText = (file: File): Promise<string> => {
     reader.onerror = () => reject(new Error('Error al leer el archivo'))
     reader.readAsText(file)
   })
+}
+
+export const parseSchemaString = (
+  jsonString: string
+): { schema: RJSFSchema | null; uiSchema: UiSchema | null } => {
+  try {
+    const parsed = JSON.parse(jsonString)
+
+    return {
+      schema: parsed.schema ?? parsed ?? null,
+      uiSchema: parsed.uiSchema ?? null,
+    }
+  } catch (error) {
+    console.error("ERROR AL PARSEAR EL SCHEMA:", error)
+    return {
+      schema: null,
+      uiSchema: null,
+    }
+  }
 }
