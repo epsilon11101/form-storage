5ce0a141b177ef09cc64fb0606be872b1e3d690e
sa101129 <sw.frontend.developer@gm.traza.digital>
2025-10-13 13:45:59 -0600

    fix: remove unnecesary buttons

--- src/components/pages/main/Header.tsx
+++ src/components/pages/main/Header.tsx
@@ -1,48 +1,26 @@
-import { Stack, TextField, Button } from "@mui/material"
+import { Stack, Button } from "@mui/material"
 
-import { useState, type ChangeEvent, type KeyboardEvent } from "react"
-import DeleteIcon from '@mui/icons-material/Delete';
-import EditIcon from '@mui/icons-material/Edit';
+
 import SaveIcon from '@mui/icons-material/Save';
 import PrintIcon from '@mui/icons-material/Print';
 
 
 const Header = () => {
 
-  const [isDisabled, setDisabled] = useState(true);
-  const [value, setValue] = useState("")
-  const valueLabel = value === "" ? "Proyecto vacio" : value;
-  const onEditHandler = () => {
-    setDisabled(false)
-  }
 
   const onPrintHandler = () => { }
-  const onDeleteHandler = () => { }
+
 
   const onSaveHandler = () => { }
 
-  const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
-    event.persist()
-    setValue(event.target.value)
-  }
-
-  const onKeyDownHandler = (event: KeyboardEvent<HTMLInputElement>) => {
-    if (event.key == "Enter") {
-      setDisabled(true)
-    }
-
-  }
-
-  const onNewProjectHandler = () => { }
 
 
   return (
     <Stack direction="row" spacing={2} >
-      <TextField placeholder={valueLabel} variant="standard" disabled={isDisabled} onChange={onChangeHandler} onKeyDown={onKeyDownHandler} value={value} />
-      <Button variant="contained" color="primary" startIcon={<EditIcon />} onClick={onEditHandler}>Renombrar</Button>
+
       <Button variant="contained" color="primary" startIcon={<SaveIcon />} onClick={onSaveHandler}>Guardar</Button>
       <Button variant="contained" color="primary" startIcon={<PrintIcon />} onClick={onPrintHandler}>Imprimir</Button>
-      <Button variant="contained" color="error" startIcon={<DeleteIcon />} onClick={onDeleteHandler}>Eliminar</Button>
+
     </Stack>
 
   )
