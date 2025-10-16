043080389d1a8658b34db2884be058fe781399ee
sa101129 <sw.frontend.developer@gm.traza.digital>
2025-10-16 17:20:18 -0600

    fix: refactor as reuzable component

--- src/components/ui/TProgress.tsx
+++ src/components/ui/TProgress.tsx
@@ -1,14 +1,41 @@
 import { CircularProgress, type CircularProgressProps } from "@mui/material"
 import Stack, { type StackProps } from "@mui/material/Stack"
-import type { FC } from "react"
+import { type FC, type ReactNode, } from "react"
 
-interface TProgressProps extends StackProps {
+
+
+
+
+interface TProgressProps extends Omit<StackProps, "children"> {
   progressProps?: CircularProgressProps
+  children?: ReactNode
+  isLoading?: boolean
+}
+interface ProgressProps extends Omit<TProgressProps, "children" | "isLoading"> { }
+
+export const TProgress: FC<TProgressProps> = ({ progressProps, children, isLoading, ...rest }) => {
+
+  const progress = <Progress progressProps={progressProps} {...rest} />
+
+
+  if (!children) {
+    return <>{progress}</>
+  }
+
+  return <>
+    {isLoading ?
+      <>{progress}</>
+      :
+      <>
+        {children}
+      </>
+    }
+  </>
+
 }
 
 
-export const TProgress: FC<TProgressProps> = ({ progressProps, ...rest }) => {
-
+const Progress: FC<ProgressProps> = ({ progressProps, ...rest }) => {
   return <Stack
     direction="column"
     justifyContent="center"
@@ -24,6 +51,6 @@ export const TProgress: FC<TProgressProps> = ({ progressProps, ...rest }) => {
       sx={{ color: (theme) => theme.palette.tertiary.main }}
       {...progressProps}
     />
-  </Stack>
 
+  </Stack>
 }
