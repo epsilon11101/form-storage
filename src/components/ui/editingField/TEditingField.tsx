4b95fc0d65a093c5ddeee085f5184237ddb624c5
sa101129 <sw.frontend.developer@gm.traza.digital>
2025-10-16 17:15:28 -0600

    feature: implemente clickawaylistener

--- src/components/ui/editingField/TEditingField.tsx
+++ src/components/ui/editingField/TEditingField.tsx
@@ -1,21 +1,29 @@
 import { TextField, type TextFieldProps } from "@mui/material";
 import { type FC, type SyntheticEvent } from "react";
+import ClickAwayListener from '@mui/material/ClickAwayListener';
 
 
 type TEditingFieldProps = {
-
+  onCancel?: () => void
 } & TextFieldProps
 
 
-export const TEditingField: FC<TEditingFieldProps> = ({ value, onChange, onKeyDown, ...rest }) => {
+export const TEditingField: FC<TEditingFieldProps> = ({ value, onChange, onKeyDown, onCancel, ...rest }) => {
   const { sx } = rest
+
 
   const onClickHandler = (e: SyntheticEvent) => {
     e.stopPropagation()
   }
 
+  const onClickAwayListener = () => {
+    onCancel?.()
+  }
 
-  return <TextField value={value} variant="outlined" onClick={onClickHandler} onKeyDown={onKeyDown} onChange={onChange} sx={{ px: 1, py: 0, ...sx }} size="small" {...rest} />
+  return <ClickAwayListener onClickAway={onClickAwayListener}>
+    <TextField value={value} variant="outlined" onClick={onClickHandler} onKeyDown={onKeyDown} onChange={onChange} sx={{ px: 1, py: 0, ...sx }} size="small" {...rest} />
+  </ClickAwayListener>
+
 
 
 
